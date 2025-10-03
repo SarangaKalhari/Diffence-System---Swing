a8f1bc12629b806de07cc10be92da1b88a6eb880
SarangaKalhari <kalharisaranga9@gmail.com>
2025-10-03 23:34:25 +0530

    create main controller ui

--- src/deffencesystem/classes/DeffenceSystem.java
+++ src/deffencesystem/classes/DeffenceSystem.java
@@ -11,6 +11,7 @@ public class DeffenceSystem {
 
     
     public static void main(String[] args) {
+       Observerable observerable = new Observerable();
        
     }
     
---
+++ src/deffencesystem/classes/Observerable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deffencesystem.classes;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Observerable {
+    
+}
